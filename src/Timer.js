eeec9d3967d578cee1e724c3e984e819e57982f3
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 21:17:13 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,9 @@ class Timer extends PureComponent {
   }
 
   //Your code here
-  
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
   componentDidMount() {
     this.interval = setInterval(
